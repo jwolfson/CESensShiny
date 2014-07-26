6b55bfd11cd25279d8d504694cb2e33c19ce4c00
Julian Wolfson <julianw@umn.edu>
2014-07-26 09:43:26 -0500

    Major updates
    - File upload working
    - Choice of smoothing kernels
    - Sensitivity parameter range warning is now in sidebar rather than on
    plot
    - Min number of bootstrap replicates for 95% CIs set to 250

--- server.R
+++ server.R
@@ -1,46 +1,89 @@
 library(shiny)
 
-load("C:/Users/Julian/Google Drive/code library/CESensShiny/data/fake.Rdata")
-data <- fake
-#pS1Y1 <- dnorm(x.pts,mean=muS1Y1,sd=sdS1Y1)
-#pS1Y0 <- pS1Y1
-S1 <- data$S[data$Z==1]
-muS1 <- mean(S1)
-sdS1 <- sd(S1)
-muS1Y1 <- mean(data$S[data$Z==1&data$Y==1])
-sdS1Y1 <- sd(data$S[data$Z==1&data$Y==1])
-
-dS1.np <- density(S1)
-pS1.np <- approxfun(dS1.np$x,dS1.np$y)
-
-dS1Y1.np <- density(data$S[data$Z==1&data$Y==1])
-pS1Y1.np <- approxfun(dS1Y1.np$x,dS1Y1.np$y)
-
-pY1 <- mean(data$Y[data$Z==1])
-pY0 <- mean(data$Y[data$Z==0])
-x.pts <- seq(min(S1),max(S1),length.out=100)
-
 # Define server logic
 shinyServer(function(input, output) {
-    
+  
+  ## Read in the file
+  data <- reactive( {
+    inFile <- input$file
+    if(!is.null(inFile)) {
+      dat <- read.csv(inFile$datapath,header=TRUE)
+    }
+    else {
+      if(!is.null(input$dataset)) {
+        thedataset <- "fake"
+      } else {  thedataset <- input$datset}
+      load(paste0("C:/Users/Julian/Google Drive/code library/CESensShiny/data/",thedataset,".Rdata")) 
+      dat <- get(thedataset)  
+    }
+    dat })
+  
+  output$datadescription <- renderText( {
+    switch(input$dataset,
+           "fake" = "Simulated data for testing purposes",
+           "modpr8" = "Modified version of antibody titer response to PR-8 flu vaccine",
+          "modweiss" = "Modified version of antibody tier response to Weiss flue vaccine",
+          "upload" = "Custom user dataset")      
+  })
+  
+  #pS1Y1 <- dnorm(x.pts,mean=muS1Y1,sd=sdS1Y1)
+  #pS1Y0 <- pS1Y1
+  S1 <- reactive( data()$S[data()$Z==1] )
+  muS1 <- reactive( mean(S1()) )
+  sdS1 <- reactive( sd(S1()) )
+  muS1Y1 <- reactive( mean(data()$S[data()$Z==1&data()$Y==1]) )
+  sdS1Y1 <- reactive( sd(data()$S[data()$Z==1&data()$Y==1]) )
+  
+    dS1.np <- reactive( {
+      switch(input$dist,
+             "np" = density(S1(),kernel=input$kernel,adjust=input$adjust),
+             "normal" = NULL,
+             "gamma" = NULL) })
+
+    pS1.np <- reactive( {
+    switch(input$dist,
+           "np" = approxfun(x=dS1.np()$x,y=dS1.np()$y),
+           "normal" = NULL,
+           "gamma" = NULL) })
+
+    dS1Y1.np <- reactive( {
+      switch(input$dist,
+             "np" = density(data()$S[(data()$Z==1)&(data()$Y==1)],kernel=input$kernel,adjust=input$adjust),
+             "normal" = NULL,
+             "gamma" = NULL) })
+  
+    pS1Y1.np <- reactive( {
+      switch(input$dist,
+             "np" = approxfun(dS1Y1.np()$x,dS1Y1.np()$y),
+             "normal" = NULL,
+             "gamma" = NULL) })
+  
+  pY1 <- reactive( mean(data()$Y[data()$Z==1]) )
+  pY0 <- reactive( mean(data()$Y[data()$Z==0]) )
+  x.pts <- reactive( seq(min(S1()),max(S1()),length.out=100) )
+  
   doBoot <- function(data,R) {
     allCEs <- sapply(1:R,function(i) {
       rows <- sample(1:nrow(data),nrow(data),replace=TRUE)
-      dat <- data[rows,]
+      dat <- data.frame(data[rows,])
       S1 <- dat$S[dat$Z==1]
       muS1 <- mean(S1)
       sdS1 <- sd(S1)
-      muS1Y1 <- mean(dat$S[dat$Z==1&dat$Y==1])
-      sdS1Y1 <- sd(data$S[dat$Z==1&dat$Y==1])
+      #obsS1Y1 <- observe({print(sum(dat$Z==1&dat$Y==1))})
+      if(sum(dat$Z==1&dat$Y==1)>=2) {
+      muS1Y1 <- mean(dat$S[(dat$Z==1)&(dat$Y==1)])
+      sdS1Y1 <- sd(dat$S[(dat$Z==1)&(dat$Y==1)])
 
-      dS1.np <- density(S1)
-      pS1.np <- approxfun(dS1.np$x,dS1.np$y)
+      if(input$dist=="np") {
+        dS1.np <- density(S1,kernel=input$kernel,adjust=input$adjust)
+        pS1.np <- approxfun(dS1.np$x,dS1.np$y)
       
-      dS1Y1.np <- density(data$S[data$Z==1&data$Y==1])
-      pS1Y1.np <- approxfun(dS1Y1.np$x,dS1Y1.np$y)
+        dS1Y1.np <- density(dat$S[(dat$Z==1)&(dat$Y==1)],kernel=input$kernel,adjust=input$adjust)
+        pS1Y1.np <- approxfun(dS1Y1.np$x,dS1Y1.np$y)
+      }
       
-      pY1 <- mean(data$Y[dat$Z==1])
-      pY0 <- mean(data$Y[dat$Z==0])
+      pY1 <- mean(dat$Y[dat$Z==1])
+      pY0 <- mean(dat$Y[dat$Z==0])
       x.pts <- seq(min(S1),max(S1),length.out=100)
 
       gam.shape <- muS1^2/sdS1^2
@@ -48,7 +91,7 @@ shinyServer(function(input, output) {
       
       pS1 <- switch(input$dist,
                "norm" = dnorm(x.pts,mean=muS1,sd=sdS1),
-               "gamma" = dgamma(x.pts,shape=gam.shape,rate=gam.rate),
+               "gamma" = suppressWarnings(dgamma(x.pts,shape=gam.shape,rate=gam.rate)),
                 "np" = pS1.np(x.pts) )
       
       gam.shape <- muS1Y1^2/sdS1Y1^2
@@ -56,7 +99,7 @@ shinyServer(function(input, output) {
         
       pS1Y1 <- switch(input$dist,
                "norm" = dnorm(x.pts,mean=muS1Y1,sd=sdS1Y1),
-               "gamma" = dgamma(x.pts,shape=gam.shape,rate=gam.rate),
+               "gamma" = suppressWarnings(dgamma(x.pts,shape=gam.shape,rate=gam.rate)),
                       "np" = pS1Y1.np(x.pts))
       
       shift.mu <- muS1+input$meanshift*sdS1
@@ -66,73 +109,85 @@ shinyServer(function(input, output) {
       
       pS1Y0 <- switch(input$dist, 
              "norm" = dnorm(x.pts,mean=shift.mu,sd=scale.sd),
-             "gamma" = dgamma(x.pts,shape=gam.shape,rate=gam.rate),
+             "gamma" = suppressWarnings(dgamma(x.pts,shape=gam.shape,rate=gam.rate)),
               "np" = pS1.np(sdS1/scale.sd*(x.pts - shift.mu)+muS1) )
       
-      CE <- pS1Y1*pY1/pS1 - pS1Y0*pY0/pS1  
+      CE <- pS1Y1*pY1/pS1 - pS1Y0*pY0/pS1
+      } else { CE <- rep(NA,length(x.pts)) }
       return(CE)
     })
     return(allCEs)
   }
   
   pS1 <- reactive( {
-    gam.shape <- muS1^2/sdS1^2
-    gam.rate <- muS1/sdS1^2
+    gam.shape <- muS1()^2/sdS1()^2
+    gam.rate <- muS1()/sdS1()^2
 
     switch(input$dist,
-           "norm" = dnorm(x.pts,mean=muS1,sd=sdS1),
-           "gamma" = dgamma(x.pts,shape=gam.shape,rate=gam.rate),
-           "np" = pS1.np(x.pts))
+           "norm" = dnorm(x.pts(),mean=muS1(),sd=sdS1()),
+           "gamma" = suppressWarnings(dgamma(x.pts(),shape=gam.shape,rate=gam.rate)),
+           "np" = pS1.np()(x.pts()))
   })
 
   pS1Y1 <- reactive( {
-    gam.shape <- muS1Y1^2/sdS1Y1^2
-    gam.rate <- muS1Y1/sdS1Y1^2
-    
+    gam.shape <- muS1Y1()^2/sdS1Y1()^2
+    gam.rate <- muS1Y1()/sdS1Y1()^2
     switch(input$dist,
-           "norm" = dnorm(x.pts,mean=muS1Y1,sd=sdS1Y1),
-           "gamma" = dgamma(x.pts,shape=gam.shape,rate=gam.rate),
-           "np" = pS1Y1.np(x.pts))
+           "norm" = dnorm(x.pts(),mean=muS1Y1(),sd=sdS1Y1()),
+           "gamma" = suppressWarnings(dgamma(x.pts(),shape=gam.shape,rate=gam.rate)),
+           "np" = pS1Y1.np()(x.pts()) )
   })
   
   update.pS1Y0 <- reactive( {
     
-    shift.mu <- muS1+input$meanshift*sdS1
-    scale.sd <- sdS1*input$sdshift
+    shift.mu <- muS1()+input$meanshift*sdS1()
+    scale.sd <- sdS1()*input$sdshift
     gam.shape <- shift.mu^2/scale.sd^2
     gam.rate <- shift.mu/scale.sd^2
     
     
     switch(input$dist, 
-      "norm" = dnorm(x.pts,mean=shift.mu,sd=scale.sd),
-      "gamma" = dgamma(x.pts,shape=gam.shape,rate=gam.rate),
-      "np" = pS1.np(sdS1/scale.sd*(x.pts - shift.mu)+muS1))
+      "norm" = dnorm(x.pts(),mean=shift.mu,sd=scale.sd),
+      "gamma" = dgamma(x.pts(),shape=gam.shape,rate=gam.rate),
+      "np" = pS1.np()(sdS1()/scale.sd*(x.pts() - shift.mu)+muS1()))
   })
-    
+
+  CEval <- reactive( {
+    pS1Y1()*pY1()/pS1() - update.pS1Y0()*pY0()/pS1()    
+  })
+  
+  output$OOB <- renderText( {
+    cevals <- CEval()
+    if(all(is.na(cevals))) {
+      a <- "WARNING: Chosen sensitivity parameters result in estimated causal effect outside [-1,1]"
+    } else if(min(na.omit(cevals))< (-1) | max(na.omit(cevals)) > 1) {
+      a <- "WARNING: Chosen sensitivity parameters result in estimated causal effect outside [-1,1]"
+    } else { a <- "" } 
+    a
+  })
+  
   output$dens <- renderPlot( {
-    hist(S1,probability=TRUE)
-    lines(x.pts,pS1(),lwd=2)
-    lines(x.pts,update.pS1Y0(),col="red")
-    
+    hist(S1(),probability=TRUE,main="Histogram of S(1)",xlab="S(1)")
+    lines(x.pts(),pS1(),lwd=2)
+    lines(x.pts(),update.pS1Y0(),col="red")
   })
   
   output$CE <- renderPlot( {
-    CE <- pS1Y1()*pY1/pS1() - update.pS1Y0()*pY0/pS1()
-    plot(x.pts,CE,type="l",main="Causal Effect = E(Y(1)-Y(0) | S(1))",xlab="S(1)",ylab="CE",ylim=input$yl)
+    plot(x.pts(),CEval(),type="l",lwd=2,
+         main="Causal Effect = E(Y(1)-Y(0) | S(1))",
+         xlab="S(1)",ylab="Causal Effect",ylim=input$yl)
     abline(h=0)
-    if(any(abs(na.omit(CE))>1)) {
-      text(quantile(x.pts,0.1),mean(CE),"WARNING: Specified sensitivity parameters result in estimated CE outside [-1,1]",col="red",adj=0,cex=1.3) }
     if(input$boot) { 
-      boot.CEs <- doBoot(data,input$n.boot)
-      boot.SEs <- apply(boot.CEs,1,sd,na.rm=TRUE)
-      boot.L <- CE - 1.96*boot.SEs
-      boot.U <- CE + 1.96*boot.SEs
-      ##boot.L <- apply(boot.CEs,1,quantile,probs=0.025,na.rm=TRUE)
-      ##boot.U <- apply(boot.CEs,1,quantile,probs=0.975,na.rm=TRUE)
-      lines(x.pts,boot.L,lty="dashed")
-      lines(x.pts,boot.U,lty="dashed")
+      boot.CEs <- doBoot(data(),input$n.boot)
+      #boot.SEs <- apply(do.call(rbind,boot.CEs),2,sd,na.rm=TRUE)
+      #boot.L <- CE - 1.96*boot.SEs
+      #boot.U <- CE + 1.96*boot.SEs
+      boot.L <- apply(do.call(rbind,boot.CEs),2,function(x) { quantile(na.omit(x),0.025)})
+      boot.U <- apply(do.call(rbind,boot.CEs),2,function(x) { quantile(na.omit(x),0.975)})
+      lines(x.pts(),boot.L,lty="dashed")
+      lines(x.pts(),boot.U,lty="dashed")
   }
 
-  })  
-
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -7,19 +7,43 @@ shinyUI(pageWithSidebar(
   headerPanel("CESens"),
   
   sidebarPanel(
-    fileInput("file","Select data file"),
-    selectInput("dist", "Distribution of S(1)",
-                list("Normal" = "norm", 
+    selectInput("dataset", "Dataset",selected="fake",
+                choices=list("Simulated" = "fake", 
+                             "Modified PR-8 vax" = "modpr8",
+                             "Modified Weiss vax" = "modweiss",
+                             "Upload CSV..." = "upload")),
+    textOutput("datadescription"),
+    conditionalPanel(
+      condition = "input.dataset =='upload'",
+      fileInput("file","Select data file"),
+      tags$p('CSV file containing columns named Z, S, and Y')),
+    tags$hr(),
+    selectInput("dist", "Distribution of S(1)",selected="norm",
+                choices=list("Normal" = "norm", 
                      "Gamma" = "gamma",
                       "Nonparametric" = "np")),
+    conditionalPanel(
+        condition = "input.dist == 'np'",
+        selectInput("kernel","Smoothing kernel:",selected="gaussian",
+                    choices=list("Gaussian" = "gaussian",
+                                 "Epanechnikov" = "epanechnikov",
+                                 "Rectangular" = "rectangular",
+                                 "Triangular" = "triangular",
+                                 "Cosine" = "cosine",
+                                 "OptCosine" = "optcosine")),
+        sliderInput("adjust","Bandwidth adjustment:",0.1,5,1,step=0.05)),
+    
+    tags$hr(),
     sliderInput("meanshift","Mean shift of S(1)|Y(0)=1, in SD",-5,5,0,step=0.05),
     sliderInput("sdshift","SD scale of S(1)|Y(0)=1",0.1,10,1,step=0.05),
-    checkboxInput("boot","Compute Bootstrap CIs",value=FALSE),
-    numericInput("n.boot","Number of bootstrap resamples",value=500),
+    strong(textOutput("OOB")),
+    tags$hr(),
+    checkboxInput("boot","Compute 95% Bootstrap CIs",value=FALSE),
+    numericInput("n.boot","Number of bootstrap resamples (min. 250)",value=500,min=250),
+    tags$hr(),
     sliderInput("yl","Display range of CE plot",-1,1,c(-1,1),step=0.05)
   ),
   
-  # Show the caption and plot of the requested variable against mpg
   mainPanel(
     plotOutput("dens"),
     plotOutput("CE")
